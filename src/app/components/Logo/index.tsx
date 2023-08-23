b9fe5b7f002e4e6635582357b1d4450fb5be6852
Argoo Argo <argooargo@Argoos-MacBook-Air.local>
2023-08-23 13:51:17 +0700

    Arthera FE

---
+++ src/app/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+import { Images } from "app/constants"
+import styled from "@emotion/styled"
+
+export const Logo = () => {
+  return <Icon src={Images.xLogoWithName} />
+}
+
+const Icon = styled.img``
